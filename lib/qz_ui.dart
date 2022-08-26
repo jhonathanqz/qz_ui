39f49bbc6f4bfa9c33c6e7efddf947df31ae6476
jhonathanqz <jhonathanqz011@gmail.com>
2022-08-26 15:33:32 -0300

    First commit

--- lib/qz_ui.dart
+++ lib/qz_ui.dart
@@ -4,6 +4,37 @@ import 'package:flutter/material.dart';
 
 import 'package:qz_ui/widgets/button/button_primary.dart';
 
+class QzUI {
+  static Widget buttonUI({
+    VoidCallback? onTap,
+    required String title,
+    bool loading = false,
+    Color? colorButton,
+    Color? colorText,
+    bool elevation = false,
+    bool isEnabled = true,
+    bool isEnabledIcon = false,
+    IconData? iconButton,
+    Color? colorIcon,
+    VoidCallback? onLongPress,
+    VoidCallback? onDoubleTap,
+  }) =>
+      ButtonPrimary(
+        title: title,
+        onTap: onTap,
+        loading: loading,
+        colorButton: colorButton,
+        colorText: colorText,
+        elevation: elevation,
+        isEnabled: isEnabled,
+        isEnabledIcon: isEnabledIcon,
+        iconButton: iconButton,
+        colorIcon: colorIcon,
+        onLongPress: onLongPress,
+        onDoubleTap: onDoubleTap,
+      );
+}
+
 Widget buttonUI({
   VoidCallback? onTap,
   required String title,
@@ -33,4 +64,4 @@ Widget buttonUI({
       onDoubleTap: onDoubleTap,
     );
 
-LoadingUI() => CircularProgressIndicator();
+Widget loadingUI() => CircularProgressIndicator();
